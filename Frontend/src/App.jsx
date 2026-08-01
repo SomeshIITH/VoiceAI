ffa2b23789ed7fe4c28084d013e3e920ea158b64
SomeshIITH <shivjaiswal51230@gmail.com>
2026-08-01 12:16:58 +0530

    added Login and Home page with help of react-route-dom now making Login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+    </Routes>
+    </>
   )
 }
 
